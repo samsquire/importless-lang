4903f8fc6fd51b7059dbb19d10073e0fc7e42ce1
Samuel Michael Squire <sam@samsquire.com>
2020-03-15 14:55:55 +0000

    Sam add pip support

--- create-lookups.py
+++ create-lookups.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 import yaml
-def add_imports(data, name, library):
+def add_imports(data, name, library, pip=None):
     offerings = dir(library)
     for offering in offerings:
         data[offering] = {"package": name}
+        if pip:
+            data[offering]["pip"] = pip
 
 data = {}
 
 import subprocess
 import flask
 add_imports(data, "subprocess", subprocess)
-add_imports(data, "flask", flask)
+add_imports(data, "flask", flask, "flask")
 
 open("lookups.yml", "w").write(yaml.dump(data))
 
--- preporter.py
+++ preporter.py
@@ -1,6 +1,7 @@
 import yaml
 import sys
 import global_finder
+from subprocess import Popen, PIPE
 
 found_globals, tree = global_finder.find_globals(sys.stdin)
 
@@ -11,6 +12,10 @@ for found in found_globals:
     if found in data:
         package = data[found]["package"]
         print("from {} import {}".format(package, found))
+        if "pip" in data[found]:
+            pip = Popen(["/usr/bin/pip3", "install",  "{}".format(package)], stdout=PIPE, stderr=PIPE)
+            pipout, piperror = pip.communicate()
+            sys.stderr.write(pipout.decode('utf-8'))
+            sys.stderr.write(piperror.decode('utf-8'))
     else:
         print("import {}".format(found))
-
